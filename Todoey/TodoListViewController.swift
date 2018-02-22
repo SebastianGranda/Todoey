3920abe5db3ff62105ae4a12f861ca603495df4b
Sebastian Granda Altamirano <sebastian.grandaa@icloud.com>
2018-02-22 10:29:09 -0500

    Add new "task" in the todoList: Pop-up alert, text field, append in the array and show up in the tableView

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 
-// 0. Link this .swift file to the table view and change the inheritate class
+// 0.- Link this .swift file to the table view and change the inheritate class
 class TodoListViewController: UITableViewController {
     
-    // 1. Items displayed
-    let itemArray = ["Find love", "Exercise", "Hey"]
+    // 1.- Items displayed
+    var itemArray = ["Find love", "Exercise", "Hey"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,12 +20,12 @@ class TodoListViewController: UITableViewController {
     
     //MARK - TableView Datasource Methods
     
-    // 2. Create the rows
+    // 2.- Create the rows
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
     
-    // 3. Display the text in the rows
+    // 3.- Display the text in the rows
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath) //Identifier: The name you gave to the navig. item in MS
         cell.textLabel?.text = itemArray [indexPath.row]
@@ -34,7 +34,7 @@ class TodoListViewController: UITableViewController {
     
     //MARK - TableView Delegate Methods
     
-    // 4. Detect when the user click in a cell
+    // 4.- Detect when the user click in a cell
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row]) //print the string in the array
         
@@ -49,6 +49,38 @@ class TodoListViewController: UITableViewController {
         }
     }
     
+    //MARK: Add new items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+       // 5.- Add the Add Pop-up to create a new object
+        
+        var textField = UITextField()
+        
+        // Create the alert
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        //What will happen once the user clicks the Add Item Button on the UIAlert
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            print("Success!")
+            
+            //Append the item written to the array
+            self.itemArray.append(textField.text!)
+            
+            //Show the new item in the table view
+            self.tableView.reloadData()
+        }
+        
+        // Create the text field where the user will write the task
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        // Show the alert
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
 
